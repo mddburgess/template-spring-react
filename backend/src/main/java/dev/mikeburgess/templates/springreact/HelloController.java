c710b92b7add2600e26612f6d53de4498daf3451
Mike Burgess <mddburgess@users.noreply.github.com>
2020-06-27 13:44:02 -0700

    Update controller to get message from repository.

--- backend/src/main/java/dev/mikeburgess/templates/springreact/HelloController.java
+++ backend/src/main/java/dev/mikeburgess/templates/springreact/HelloController.java
@@ -1,13 +1,19 @@
 package dev.mikeburgess.templates.springreact;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import dev.mikeburgess.templates.springreact.repository.MessageRepository;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private MessageRepository messageRepository;
+
     @GetMapping("/api/hello")
     public String getHello() {
-        return "Hello World!";
+        return messageRepository.getOne(1).getMessage();
     }
 }
--- backend/src/test/java/dev/mikeburgess/templates/springreact/HelloControllerTest.java
+++ backend/src/test/java/dev/mikeburgess/templates/springreact/HelloControllerTest.java
@@ -3,8 +3,13 @@ package dev.mikeburgess.templates.springreact;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import dev.mikeburgess.templates.springreact.entity.Message;
+import dev.mikeburgess.templates.springreact.repository.MessageRepository;
+
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -15,8 +20,16 @@ class HelloControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private MessageRepository messageRepository;
+
     @Test
     void getHello() throws Exception {
+        var message = new Message();
+        message.setMessage("Hello World!");
+
+        given(messageRepository.getOne(1)).willReturn(message);
+
         mockMvc.perform(get("/api/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello World!"));
