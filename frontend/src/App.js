e9478d91093b1ae75dc6fb80fba13554f5456087
Mike Burgess <mddburgess@users.noreply.github.com>
2020-06-27 08:59:01 -0700

    Fetch hello world message.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState(null);
+
+    async function fetchMessage() {
+        const response = await fetch('api/hello');
+        setMessage(await response.text());
+    }
+
+    useEffect(() => {
+        fetchMessage();
+    });
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>{message}</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
